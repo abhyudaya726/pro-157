f7b58cad1b367c610d736a193b3483ad5bd50174
abhyudaya726 <71880557+abhyudaya726@users.noreply.github.com>
2022-01-28 19:00:13 +0530

    Add files via upload

---
+++ js/Tour.js
@@ -0,0 +1,96 @@
+AFRAME.registerComponent("tour",{
+    init:function() {
+        this.placesContainer = this.el;
+        this.createCards();
+    },
+    createCards: function() {
+    
+        const thumbNailsRef = [
+          {
+            id: "captian-aero",
+            title: "Captian Aero",
+            url: "./assets/assets/posters/captain-aero-poster.jpg"
+          },
+          {
+            id: "outer-space",
+            title: "Outer Space",
+            url: "./assets/assets/posters/outer-space-poster.jpg"
+          },
+    
+          {
+            id: "spiderman",
+            title: "Spider Man",
+            url: "./assets/assets/posters/spiderman-poster.jpg"
+          },
+          {
+            id: "superman",
+            title: "SuperMan",
+            url: "./assets/assets/posters/superman-poster.jpg"
+          }
+        ];
+        let prevoiusXPosition = -60;
+        for (var item of thumbNailsRef) {
+          const posX = prevoiusXPosition + 25;
+          const posY = 10;
+          const posZ = -40;
+          const position = { x: posX, y: posY, z: posZ };
+          prevoiusXPosition = posX;
+    
+          // Border Element
+          const borderEl = this.createBorder(position, item.id);
+    
+          // Thubnail Element
+          const thumbNail = this.createThumbNail(item);
+          borderEl.appendChild(thumbNail);
+    
+          // Title Text Element
+          const titleEl = this.createTitleEl(position, item);
+          borderEl.appendChild(titleEl);
+    
+          this.placesContainer.appendChild(borderEl);
+        }
+      },
+      createBorder: function(position, id) {
+        const entityEl = document.createElement("a-entity");
+        entityEl.setAttribute("id", id);
+        entityEl.setAttribute("visible", true);
+        entityEl.setAttribute("geometry", {
+          primitive: "ring",
+          radiusInner: 9,
+          radiusOuter: 10
+        });
+        entityEl.setAttribute("position", position);
+        entityEl.setAttribute("material", {
+          color: "black",
+          opacity: 1
+        });
+        entityEl.setAttribute("cursor-listener", {});
+        return entityEl;
+      },
+      createThumbNail: function(item) {
+        const entityEl = document.createElement("a-entity");
+        entityEl.setAttribute("visible", true);
+        entityEl.setAttribute("geometry", {
+          primitive: "circle",
+          radius: 9
+        });
+        entityEl.setAttribute("material", { src: item.url });
+        entityEl.setAttribute("cursor-listener", {});
+        return entityEl;
+      },
+      createTitleEl: function(position, item) {
+        const entityEl = document.createElement("a-entity");
+        entityEl.setAttribute("text", {
+          font: "exo2bold",
+          align: "center",
+          width: 60,
+          color: "#e65100",
+          value: item.title
+        });
+        const elPosition = position;
+        elPosition.y = -20;
+        entityEl.setAttribute("position", elPosition);
+        entityEl.setAttribute("visible", true);
+        return entityEl;
+      }, 
+})
